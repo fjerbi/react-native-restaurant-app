f21dae99fd9e12a2a8dead56962398caa854838a
fjerbi <firasjerbiv2@gmail.com>
2021-10-22 15:29:13 +0100

    Addin navigation and search bar + styling

---
+++ App.js
@@ -0,0 +1,18 @@
+
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+import SearchScreen from "./src/screens/SearchScreen";
+
+const navigator = createStackNavigator(
+  {
+    Search: SearchScreen,
+  },
+  {
+    initialRouteName: "Search",
+    defaultNavigationOptions: {
+      title: "Business Search",
+    },
+  }
+);
+
+export default createAppContainer(navigator);
